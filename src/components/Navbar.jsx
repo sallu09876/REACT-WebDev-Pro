82e6fdaf94dab1f130fb945f2648e23bf05fc52d
Salman Rasheed <sallu2004mkt@gmail.com>
2025-11-14 09:50:51 +0530

    latest

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
-import '../App.css';
 
 function Navbar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
